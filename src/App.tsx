79349e2217bf25ede760cd7eed9f848116686f52
ConradGuillaume <conrad.guillaume0501@gmail.com>
2023-10-21 17:22:18 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import MainPage from "./components/MainPage";
 
 function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const hours = String(time.getHours()).padStart(2, "0");
+  const minutes = String(time.getMinutes()).padStart(2, "0");
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>MOI</h1>
+        <h2>{`${hours}:${minutes}`}</h2>
+        <MainPage />
       </header>
     </div>
   );
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Screen1 from "./Screen1";
+
+export default function MainPage() {
+  return (
+    <div>
+      <Screen1 />
+    </div>
+  );
+}
